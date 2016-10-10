5b039d11f4e41251c05ff9bd32820a9ef90a0da0
Aidan <sliney@spotify.com>
2016-10-10 22:10:19 +0200

    Complete image slider

---
+++ Draw/TutorialCollectionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TutorialCollectionViewController.swift
+//  Draw
+//
+//  Created by Aidan Sliney on 09/10/2016.
+//  Copyright © 2016 Aidan Sliney. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class TutorialCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
