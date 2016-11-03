1f5c1a7d302db467f2504b3fb56aabbdbf420385
Aidan <sliney@spotify.com>
2016-11-03 22:43:21 +0100

    Added Tutorial

--- Draw/FirstViewController.swift
+++ Draw/FirstViewController.swift
@@ -11,10 +11,13 @@ import AVKit
 import AVFoundation
 
 class FirstViewController: UIViewController {
+    
+    var tutorial:[Tutorial] = book0Tutorials
 
     @IBOutlet weak var welcome: UILabel!
     @IBOutlet weak var playerView: UIView!
     @IBOutlet weak var playVideo: UIImageView!
+    @IBOutlet weak var tutorialImage: UIImageView!
     
     @IBOutlet weak var topImage: UIImageView!
     override func viewDidLoad() {
@@ -27,6 +30,9 @@ class FirstViewController: UIViewController {
         
         let tapGestureVideo = UITapGestureRecognizer(target: self, action: #selector(handleTapVideo))
         playVideo.addGestureRecognizer(tapGestureVideo)
+        
+        let tapGestureTutorial = UITapGestureRecognizer(target: self, action: #selector(handleTapTutorial))
+        tutorialImage.addGestureRecognizer(tapGestureTutorial)
         
         // Do any additional setup after loading the view.
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 38, height: 38))
@@ -49,6 +55,11 @@ class FirstViewController: UIViewController {
         print("tap")
     }
     
+    func handleTapTutorial(sender: UITapGestureRecognizer) {
+        performSegue(withIdentifier: "toTutorialDetailFromTutCardSeg", sender: sender)
+        print("tap")
+    }
+    
     func handleTapVideo(sender: UITapGestureRecognizer) {
         print("tapVideo")
         let videoURL = NSURL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
@@ -61,7 +72,7 @@ class FirstViewController: UIViewController {
     }
     
     
-    //preparing for segue
+    //preparing for segue to book
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "newToBookDetailSeg" {
             let destinationVC = segue.destination as! BookDetailController
@@ -69,10 +80,18 @@ class FirstViewController: UIViewController {
             destinationVC.passedLevel = bookData[1].level
             destinationVC.passedBookname = bookData[1].bookname
             destinationVC.passedH1 = bookData[1].h1
-            
+            destinationVC.passedBanner = bookData[1].banner
             destinationVC.passedTutorial = bookData[1].tutorial
         }
+        //Tutorial segue
+        if segue.identifier == "toTutorialDetailFromTutCardSeg" {
+            let destinationVC2 = segue.destination as! TutorialViewController
+            
+            let tutorial = self.tutorial[0] as Tutorial
+            destinationVC2.passedBook = tutorial.book!
+            destinationVC2.passedT = tutorial.t!
+            destinationVC2.passedLength = tutorial.length!
+        }
     }
-
 }
 
